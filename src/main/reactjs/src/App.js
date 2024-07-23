43bc117db70f78152921abe2d8a4208524515f8c
JUYONG0133 <qwer99633@naver.com>
2024-07-23 16:59:59 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import mainPhoto from './K-040.png';
+function App() {
+  return (
+    <div className="App">
+      <img alt='main'
+        src={mainPhoto}
+           style={{width:'300px',border:'5px inset gray'}}
+      />
+      <h3><b>배포</b></h3>
+    </div>
+  );
+}
+
+export default App;
